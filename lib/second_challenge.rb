6474482a94d8a2c9bff89b1c827b6f58a294c931
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-02 00:16:01 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,20 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => [
+        {name: "Benvolio", age: "17", attitude: "worried"},
+        {name: "Steven", age: "30", attitude: "confused"}]
       }, 
     :capulet => {
-      #your key/value pairs here!
-      }}
-
-  
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => [
+        {name: "Mercutio", age: "18", attitude: "hot-headed"}, 
+        {name: "Nurse", age: "44", attitude: "worried"}]
+      }
+    }
 end
